e10ed83d20640ad1dcdf45b1aa57cc4acf6b4528
Ribeigu <gustavo.r.mota06@gmail.com>
2025-06-12 12:23:12 -0300

    Criação de Reservas

---
+++ BibliotecaES1/src/Aplication/CatalogoReserva.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aplication;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gusta
+ */
+public class CatalogoReserva {
+    private ArrayList<Reserva> reservas;
+    
+    public Reserva busca(int posicao){
+        return reservas.get(posicao);
+    }
+    
+    public int tamanhoReservas(){
+        return reservas.size();
+    }
+    
+    public void adicionarReserva(Reserva reserva){
+        reservas.add(reserva);
+    }
+}
---
+++ BibliotecaES1/src/Aplication/Reserva.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aplication;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ *
+ * @author gusta
+ */
+public class Reserva {
+    private Livro livro;
+    private LocalDate dataExpiracao;
+    private Leitor leitor;
+
+    public Reserva(Livro livro, Leitor leitor) {
+        this.livro = livro;
+        this.dataExpiracao = LocalDate.now();
+        this.leitor = leitor;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    public String getDataExpiracao() {
+        return dataExpiracao.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
+
+    public void setDataExpiracao(LocalDate dataExpiracao) {
+        this.dataExpiracao = dataExpiracao;
+    }
+
+    public Leitor getLeitor() {
+        return leitor;
+    }
+
+    public void setLeitor(Leitor leitor) {
+        this.leitor = leitor;
+    }
+    
+    public void criarReserva(Leitor leitor, Livro livro ){
+        Reserva reserva = new Reserva(leitor, livro);
+        CatalogoReserva.adicionarReserva(reserva);  
+    }
+}
