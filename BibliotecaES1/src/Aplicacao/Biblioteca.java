2fe87344271094b4f9d3d7dbee5bde7b4a3a01f9
Ribeigu <gustavo.r.mota06@gmail.com>
2025-07-02 02:33:32 -0300

    .' '

--- BibliotecaES1/src/Aplicacao/Biblioteca.java
+++ BibliotecaES1/src/Aplicacao/Biblioteca.java
@@ -3,13 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package Aplicacao;
-
+import Interface.TelaBiblioteca;
+import javax.swing.SwingUtilities;
 /**
  *
  * @author gusta
  */
 public class Biblioteca {
-
+import Interface.TelaBiblioteca;
     /**
      * @param args the command line arguments
      */
@@ -17,6 +18,16 @@ public class Biblioteca {
         // TODO code application logic here
         var Biblioteca = ControladorBiblioteca.getInstancia();
         Biblioteca.Instanciar();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                // Cria uma instância da sua tela
+               TelaBiblioteca tela = new TelaBiblioteca();
+                
+                // Torna a tela visível
+                tela.setVisible(true);
+            }
+        });
     }
     
 }
